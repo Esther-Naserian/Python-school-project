ac2bcffb203f9f2445f7c275152ee6d580813c66
Esther-Naserian <naseriansamuria@gmail.com>
2024-07-17 15:17:34 +0300

    commit changes

--- classperiod/models.py
+++ classperiod/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Classperiod(models.Model):
-    class_start_time=models.DurationField()
-    class_end_time=models.DurationField()
-    class_courses_course=models.CharField(max_length=20)
-    classroom_class=models.CharField(max_length=20)
-    day_of_the_week=models.CharField(max_length=20)
+    class_start_time = models.DurationField()
+    class_end_time = models.DurationField()
+    class_courses_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='class_periods')
+    classroom_class = models.OneToOneField(Classroom, on_delete=models.CASCADE, related_name='class_period')
+    day_of_the_week = models.ManyToManyField(DayOfWeek, related_name='class_periods')
 
-def __str__(self):
-        return f"{self.class_start_time} {self.class_end_time}"
+    def __str__(self):
+        return f"{self.class_start_time} - {self.class_end_time}"
